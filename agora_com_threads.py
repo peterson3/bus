bb4b0e69af76e6fe30e469329ee4a4241d48f2e2
Natalia Rocha de Almeida Pipas <npipas@br.ibm.com>
2016-10-16 20:04:27 -0200

    carrega_grid

--- agora_com_threads.py
+++ agora_com_threads.py
@@ -9,8 +9,9 @@ import urllib
 import time
 import os
 from collections import deque
+import threading
 
-dict = {}
+dict = {} #guarda ultimas posicoes dos onibus 
 
 conf = SparkConf()
 
@@ -21,7 +22,11 @@ sqlContext = HiveContext(sc)
 
 sqlContext.setConf("hive.exec.dynamic.partition.mode", "nonstrict")
 
-num_pontos = 5
+num_pontos = 5 #numero de pontos para olhar para tras
+
+grid = {} #guarda posicoes das trajetorias ja conhecidas
+
+div = 10000 #numero pelo qual dividir lats e longs
 
 def get_dados():
 	while(True):
@@ -49,7 +54,23 @@ threading.Thread(target = get_dados).start()
 
 def acha_linha(pontos):
 	for ponto in pontos:
-		
+
+def carrega_linha(linha_num):
+	f = open("/home/Natalia/tcc/{linha_num}.csv".format(linha_num = linha_num))
+	f.readline()
+	for ponto in f:
+		lat = float(ponto.split(" ")[0].split("(")[1])
+		lng = float(ponto.split(" ")[1].split(")")[0])
+		grid[str(lat/div)+","+str(lng/div)] = (linha_num,lat,lng)
+
+def carrega_grid():
+	carrega_linha("422")
+	carrega_linha("298")
+	#rot = sc.textFile("file:///home/Natalia/tcc/422.csv")
+	#rflt = rot.filter(lambda x: len(x) > 11) #elimina header
+	#rmp = rflt.map(lambda x: ("864",(float(x.split(" ")[1].split(")")[0]),float(x.split(" ")[0].split("(")[1])))).cache()
+
+carrega_grid()
 
 while(True):
 	for ordem in dict:
